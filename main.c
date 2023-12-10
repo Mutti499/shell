53250af228c1325026470dd2a7df3d6ad35cf5de
Mustafa Atak <mustafa.atak@boun.edu.tr>
2023-12-10 20:13:52 +0300

    background proogram counting done alias altered

--- main.c
+++ main.c
@@ -10,27 +10,20 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-
 #define MAX_COMMAND_LENGTH 1024
 #define READ_END 0
 #define WRITE_END 1
-#define BUFFER_SIZE 500
+#define BUFFER_SIZE 1024
 
-char aliasses[64][256];       // 64 strings, each can hold 255 characters + null terminator
-char alias_commands[64][256]; // Similarly for command strings
+char aliasses_keys[64][256];   // 64 strings, each can hold 255 characters + null terminator
+char aliasses_values[64][256]; // Similarly for command strings
 int alias_count = 0;
+
+char last_executed_command[1024] = "";
 
 char parent_shell[256];
 
-// volatile sig_atomic_t child_process_count = 0;
-
-// void sigchld_handler(int signum) {
-//     int status;
-//     while (waitpid(-1, &status, WNOHANG) > 0) {
-//         child_process_count--;
-//     }
-// }
-
+int background_process_count = 0;
 
 // Function to split the command line into arguments
 int parse_command(char *command, char args[64][256])
@@ -63,10 +56,142 @@ void reverse_buffer(char *buffer, ssize_t count)
     }
 }
 
+char *get_current_shell_from_proc()
+{
+    memset(parent_shell, 0, sizeof(parent_shell));
+
+    pid_t ppid = getppid();
+    char shell_path[256];
+    sprintf(shell_path, "/proc/%d/comm", ppid);
+    printf("shell path: %s \n", shell_path);
+    printf("ppid: %d \n", ppid);
+
+    FILE *fp = fopen(shell_path, "r"); // Open the comm file for reading
+
+    fgets(parent_shell, sizeof(parent_shell), fp);
+    fclose(fp);
+
+    // Remove newline character if present
+    char *newline = strchr(parent_shell, '\n');
+    if (newline)
+        *newline = '\0';
+
+    return parent_shell;
+}
+
+char *executable_path_finder(char *cmd)
+{
+    char *path = getenv("PATH");
+    if (!path)
+        return NULL;
+
+    char *dirs = strdup(path);
+    if (!dirs)
+        return NULL;
+
+    // printf("dirs: %s \n", dirs); Al paths
+
+    char *dir = strtok(dirs, ":");
+    while (dir != NULL)
+    {
+
+        char *path = malloc(strlen(dir) + strlen(cmd) + 2); // +2 for the slash and null terminator
+        sprintf(path, "%s/%s", dir, cmd);
+        if (access(path, X_OK) == 0)
+        {
+            free(dirs);
+            return path; // the executable path
+        }
+
+        free(path);
+        dir = strtok(NULL, ":");
+    }
+
+    free(dirs);
+    return NULL;
+}
+
+void bello_executer()
+{
+    char *username = getenv("USER");
+    char hostname[512];
+    gethostname(hostname, 512);
+
+    char *tty = ttyname(STDIN_FILENO);
+
+    // char* shell = getenv("SHELL"); // This moight not work when there are other shells in the system it just indicates default shell
+    // char* shell = get_current_shell_from_proc();
+
+    char *home = getenv("HOME");
+
+    time_t t; // not a primitive datatype
+    time(&t);
+
+    printf("Username: %s\nHostname: %s\nLast SUCCESSFULLY Executed Command: %s\nTTY: %s\nCurrent Shell Name: %s\nHome Location: %s\nCurrent Time and Date: %sNumber of Process in Background: %d\n",
+           username, hostname, last_executed_command, tty, parent_shell, home, ctime(&t), background_process_count);
+
+    return;
+}
+// In your main function or initialization code
+void eraser(char args[64][256])
+{
+    for (int i = 0; i < 64; i++)
+    {
+        int len = strlen(args[i]);
+        bool in_quotes = false;
+        int shift_offset = 0;
+
+        for (int j = 0; j < len; j++)
+        {
+            if (args[i][j] == '"' || args[i][j] == '\'')
+            {
+                in_quotes = !in_quotes; // Toggle the in_quotes status
+                shift_offset++;         // Increase the shift offset
+            }
+            else if (shift_offset > 0)
+            {
+                args[i][j - shift_offset] = args[i][j]; // Shift characters to the left
+            }
+        }
+        args[i][len - shift_offset] = '\0'; // Null-terminate the string at the new length
+    }
+}
+
+int controller(char args[64][256])
+{
+    int single_counter = 0;
+    int double_counter = 0;
+
+    for (int i = 0; i < 64; i++)
+    {
+        for (int j = 0; args[i][j] != '\0'; j++)
+        { // end of a word
+            if (args[i][j] == '"')
+            {
+                double_counter++;
+            }
+            else if (args[i][j] == '\'')
+            {
+                single_counter++;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    if (single_counter % 2 != 0 || double_counter % 2 != 0)
+    {
+        printf("Syntax error: unbalanced quotes!\n");
+        return 1;
+    }
+    return 0;
+}
+
 // Function to execute the command
 int execute_command(char args[64][256], bool background, bool redirect1, bool redirect2, bool redirect3, char *output_file)
 {
-
     int fd[2];
     pid_t pid;
 
@@ -116,45 +241,72 @@ int execute_command(char args[64][256], bool background, bool redirect1, bool re
             dup2(file, STDOUT_FILENO);
             close(file);
         }
+        else if (redirect3 && strcmp(args[0], "bello") == 0)
+        {
+            close(fd[READ_END]);                // Close read end
+            dup2(fd[WRITE_END], STDOUT_FILENO); // Redirect stdout to write end of pipe
+            close(fd[WRITE_END]);               // Close write end
+        }
         else if (redirect3)
         {
-
             close(fd[READ_END]);                // Close read end
             dup2(fd[WRITE_END], STDOUT_FILENO); // Redirect stdout to write end of pipe
             close(fd[WRITE_END]);               // Close write end
         }
 
-        char *arg_pointers[65]; // Array of pointers
-        int j = 0;
-        for (int i = 0; i < 64 && args[i][0] != '\0'; i++)
+        if (controller(args) == 1)
         {
-            arg_pointers[j++] = args[i]; // Only copy non-empty arguments
+            _exit(1);
         }
-        arg_pointers[j] = NULL; // NULL-terminate the array
-
-        // execvp(args[0], args); // Execlp can not be used since it does not take the args array so it is not dynamic
-        if (execvp(arg_pointers[0], arg_pointers) == -1)
+        if (strcmp(args[0], "echo") == 0)
         {
-            if (errno == ENOENT)
-            {
-                fprintf(stderr, "%s: Command not found\n", arg_pointers[0]);
-            }
-            exit(EXIT_FAILURE); // Only exit the child process, not the shell itself
-        }else{
+            eraser(args);
+        }
+        if (strcmp(args[0], "bello") == 0)
+        {
+            bello_executer();
             _exit(0);
         }
-        
+        else
+        {
+            // printf("Executing %s \n", args[0]);
+            char *arg_pointers[65]; // Array of pointers
+            int j = 0;
+            for (int i = 0; i < 64 && args[i][0] != '\0'; i++)
+            {
+                arg_pointers[j++] = args[i]; // Only copy non-empty arguments
+            }
+            arg_pointers[j] = NULL; // NULL-terminate the array
+
+            // execvp(arg_pointers[0], arg_pointers); // Execlp can not be used since it does not take the args array so it is not dynamic
+
+            char *executable_path = executable_path_finder(arg_pointers[0]);
+            if (executable_path)
+            {
+                if (execv(executable_path, arg_pointers) == -1)
+                {
+                    fprintf(stderr, "Error executing %s: %s\n", executable_path, strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+                free(executable_path);
+                _exit(0);
+            }
+            else
+            {
+                fprintf(stderr, "%s: Command not found\n", arg_pointers[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
     }
     else
     { // Parent process
-
-        // child_process_count++;
 
         if (redirect3)
         {
             close(fd[WRITE_END]);
             char read_msg[BUFFER_SIZE];
             ssize_t count = read(fd[READ_END], read_msg, BUFFER_SIZE);
+            // printf("Read %ld bytes from the pipe: \"%.*s\"\n", count, (int)count, read_msg);
 
             if (count > 0)
             {
@@ -175,11 +327,17 @@ int execute_command(char args[64][256], bool background, bool redirect1, bool re
 
         if (!background)
         {
-            wait(NULL);
+            int status;
+            waitpid(pid, &status, 0); // Waits for the specific child process to complete and captures its status
+            if (WIFEXITED(status))
+            {
+                return WEXITSTATUS(status); // Return the exit status of the child process
+            }
         }
         else
         {
             // printf("Child is running in background \n");
+            background_process_count++;
         }
     }
     return 0;
@@ -189,36 +347,40 @@ void check_background()
 {
     int status;
     pid_t pid;
-    pid = waitpid(-1, &status, WNOHANG);
 
-    if (pid > 0)
+    while (pid = waitpid(-1, &status, WNOHANG) > 0)
     {
-        if (WIFEXITED(status))
+        background_process_count--;
+
+        if (pid > 0)
         {
-            int exit_status = WEXITSTATUS(status);
-            if (exit_status == 0)
+            if (WIFEXITED(status))
             {
-                printf("Child %d terminated normally\n", pid);
+                int exit_status = WEXITSTATUS(status);
+                if (exit_status == 0)
+                {
+                    printf("Child %d terminated normally\n", pid);
+                }
+                else
+                {
+                    printf("Child %d terminated with exit status %d\n", pid, exit_status);
+                }
             }
-            else
+            else if (WIFSIGNALED(status))
             {
-                printf("Child %d terminated with exit status %d\n", pid, exit_status);
+                // Child killed by a signal.
+                printf("Child %d terminated by signal %d\n", pid, WTERMSIG(status));
             }
-        }
-        else if (WIFSIGNALED(status))
-        {
-            // Child killed by a signal.
-            printf("Child %d terminated by signal %d\n", pid, WTERMSIG(status));
-        }
-        else if (WIFSTOPPED(status))
-        {
-            // Child stopped.
-            printf("Child %d stopped by signal %d\n", pid, WSTOPSIG(status));
-        }
-        else if (WIFCONTINUED(status))
-        {
-            // Child continue.
-            printf("Child %d continued\n", pid);
+            else if (WIFSTOPPED(status))
+            {
+                // Child stopped.
+                printf("Child %d stopped by signal %d\n", pid, WSTOPSIG(status));
+            }
+            else if (WIFCONTINUED(status))
+            {
+                // Child continue.
+                printf("Child %d continued\n", pid);
+            }
         }
     }
     if (pid == -1 && errno != ECHILD)
@@ -228,8 +390,12 @@ void check_background()
     return;
 }
 
-int save_alias(char *alias_command, char **alias_args)
+int save_alias(char *alias_key, char *alias_value)
 {
+    printf("alias command: %s \n", alias_key);
+    printf("alias args: %s \n", alias_value);
+    
+
     FILE *fp;
     fp = fopen("alias.txt", "a");
     if (fp == NULL)
@@ -237,23 +403,13 @@ int save_alias(char *alias_command, char **alias_args)
         perror("Error opening file");
         return -1;
     }
-    fprintf(fp, "%s = ", alias_command);
-    for (size_t i = 0; i < sizeof(alias_args); i++)
-    {
+    fprintf(fp, "%s = %s\n", alias_key, alias_value);
 
-        if (alias_args[i] == NULL)
-        {
-            break;
-        }
-
-        fprintf(fp, "%s ", alias_args[i]);
-    }
-    fprintf(fp, "\n");
     fclose(fp);
     return 0;
 }
 
-int load_aliasses()
+int load_aliasses_keys()
 {
     FILE *fp;
     char *line = NULL;
@@ -269,12 +425,14 @@ int load_aliasses()
     char command[256];
     while ((read = getline(&line, &len, fp)) != -1)
     {
-        // printf("%s", line);
-        if (sscanf(line, "%s = \"%[^\"]\"", key, command) == 2)
-        {
-            strcpy(aliasses[alias_count], key);
-            strcpy(alias_commands[alias_count], command);
 
+        // printf("%s", line);
+        if (sscanf(line, "%[^=] = %[^\n]", key, command) == 2)
+        {
+            strcpy(aliasses_keys[alias_count], key);
+            strcpy(aliasses_values[alias_count], command);
+            printf("key: %s \n", key);
+            printf("command: %s \n", command);
             alias_count++;
         }
     }
@@ -284,7 +442,7 @@ int load_aliasses()
     return 0;
 }
 
-void executor(char args[64][256], int arg_count)
+int executor(char args[64][256], int arg_count)
 {
     bool background;
     if (strcmp(args[arg_count - 1], "&") == 0)
@@ -332,11 +490,11 @@ void executor(char args[64][256], int arg_count)
         redirect2 = false;
         redirect3 = false;
     }
-    char *alias_command;
-    char *alias_args[64];
+    char *alias_key;
+    char *alias_value[64];
     if (strcmp(args[0], "alias") == 0)
     {
-        alias_command = args[1];
+        alias_key = args[1];
         char command_buffer[1024] = "";
         for (size_t i = 3; i < arg_count; i++) // after the =
         {
@@ -344,74 +502,44 @@ void executor(char args[64][256], int arg_count)
             {
                 break;
             }
-            alias_args[i - 3] = args[i];
+            alias_value[i - 3] = args[i];
             strcat(command_buffer, args[i]);
             strcat(command_buffer, " "); // Add a space between arguments
         }
-        strcpy(aliasses[alias_count], alias_command);
-        strcpy(alias_commands[alias_count], command_buffer);
+        strcpy(aliasses_keys[alias_count], alias_key);
 
-        save_alias(alias_command, alias_args);
+        int len = strlen(command_buffer);
+        int quote_count = 0;
+        int last_quote_pos = -1;
+
+        for (int i = 0; i < len; i++)
+        {
+            if (command_buffer[i] == '"')
+            {
+                last_quote_pos = i;
+                quote_count++;
+            }
+        }
+
+        command_buffer[last_quote_pos] = '#'; // Null-terminate to remove the last quote
+
+        char value[256];
+        if (sscanf(command_buffer, "\"%[^#]\"", value) != 1)
+        {
+            printf("Syntax error: unbalanced quotes!\n");
+            return 1;
+        }
+        printf("value: %s \n", value);
+        strcpy(aliasses_values[alias_count], value);
         alias_count++;
+        save_alias(alias_key, value);
+
+        return 0;
     }
     else
     {
-        // printf("%s \n", args[0]);
-        // printf("%s \n", args[1]);
-        // printf("%s \n", args[2]);
-        // printf("%s \n", args[3]);
-        execute_command(args, background, redirect1, redirect2, redirect3, output_file);
+        return execute_command(args, background, redirect1, redirect2, redirect3, output_file);
     }
-}
-
-char *get_current_shell_from_proc()
-{
-    memset(parent_shell, 0, sizeof(parent_shell));
-
-    pid_t ppid = getppid();
-    char shell_path[256];
-    printf("ppid: %d \n", ppid);
-    sprintf(shell_path, "/proc/%d/comm", ppid);
-
-    FILE *fp = fopen(shell_path, "r"); // Open the comm file for reading
-
-    fgets(parent_shell, sizeof(parent_shell), fp);
-    fclose(fp);
-
-    // Remove newline character if present
-    char *newline = strchr(parent_shell, '\n');
-    if (newline) *newline = '\0';
-
-    return parent_shell;
-}
-
-void bello_executer(char *last_executed_command)
-{
-    char *username = getenv("USER");
-    char hostname[512];
-    gethostname(hostname, 512);
-
-    char *tty = ttyname(STDIN_FILENO);
-
-    // char* shell = getenv("SHELL"); // This moight not work when there are other shells in the system it just indicates default shell
-    // char* shell = get_current_shell_from_proc();
-    if (parent_shell[0] == '\0')
-    {
-        get_current_shell_from_proc();
-    }
-
-    char *home = getenv("HOME");
-
-    time_t t; // not a primitive datatype
-    time(&t);
-
-    printf("Username: %s\n", username);
-    printf("Hostname: %s\n", hostname);
-    printf("Last Executed Command: %s\n", last_executed_command);
-    printf("TTY: %s\n", tty);
-    printf("Current Shell Name: %s\n", parent_shell);
-    printf("Home Location: %s\n", home);
-    printf("Current Time and Date: %s\n", ctime(&t));
 }
 
 void reset_args(char args[64][256])
@@ -423,23 +551,13 @@ void reset_args(char args[64][256])
     }
 }
 
-
-// In your main function or initialization code
-
 int main()
 {
-
-    // Rest of your code...
-    // struct sigaction sa;
-
-    // sa.sa_handler = sigchld_handler;
-    // sigemptyset(&sa.sa_mask);
-    // sa.sa_flags = 0;
-    // sigaction(SIGCHLD, &sa, NULL);
-
     char command[MAX_COMMAND_LENGTH];
     char args[64][256];
     char args2[64][256];
+
+    get_current_shell_from_proc();
 
     char hostname[512];
     gethostname(hostname, 512);
@@ -452,8 +570,8 @@ int main()
     char shellCommand[1030];
     sprintf(shellCommand, "%s@%s %s --- ", username, hostname, cwd);
 
-    load_aliasses();
-    char last_executed_command[1024] = "";
+    load_aliasses_keys();
+
     while (1)
     {
 
@@ -467,7 +585,6 @@ int main()
             perror("fgets failed");
             continue;
         }
-
         // Check for the 'exit' command
         if (strcmp(command, "exit\n") == 0)
         {
@@ -480,14 +597,13 @@ int main()
             continue;
         }
         check_background();
-
         for (int i = 0; i < alias_count; i++)
         {
-            if (strcmp(aliasses[i], args[0]) == 0)
+            if (strcmp(aliasses_keys[i], args[0]) == 0)
             {
 
                 char command2[1024];
-                strcpy(command2, alias_commands[i]);
+                strcpy(command2, aliasses_values[i]);
                 int arg_count2 = parse_command(command2, args2);
                 for (int k = 1; k < arg_count; k++)
                 {
@@ -506,25 +622,18 @@ int main()
                 break;
             }
         }
-        if (strcmp(args[0], "bello") == 0)
-        {
-            bello_executer(last_executed_command);
-            continue;
-        }
 
         // printf("Args1: %s %s %s %s %s %s \n", args[0], args[1], args[2], args[3], args[4], args[5]);
         // printf("arg counter : %d \n", arg_count);
-        executor(args, arg_count);
-        strcpy(last_executed_command, command);
+
+        int res = executor(args, arg_count);
+        // printf("return value: %d \n", res);
+        if (res == 0)
+        {
+            strcpy(last_executed_command, command);
+        }
     }
 }
-
-// Bello yaz
-// Syntax error tespiti yap
-// cift tirnak ayirmalari guncelle
 // rapor bak
 
-// https://moodle.boun.edu.tr/mod/forum/discuss.php?d=141800
-
-// When we print echo "message" in a regular terminal, we get <message> as output, but in my implementation i get with double quotes. Should I remove double quotes or is it okay?
-// Better if you remove
+//change alias not implemented
